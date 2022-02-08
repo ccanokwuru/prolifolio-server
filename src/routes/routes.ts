bdd4bd92a6d7e5f451a965940b5aeca1b2e6264e
Anokwuru Chisom <cccanoks@gmail.com>
2022-02-08 22:01:41 +0100

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyPluginAsync } from "fastify";
 
 const worksRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/root/*", async function (request, reply) {
-    return request.url
+  fastify.get("/", async function (request, reply) {
+    return {
+      message:"root route"
+    }
   });
 };
 
